9d6b454df229ddfe7e58c32721d2fbe32a2e8e64
SamuelK0nrad <samuel.ko702@gmail.com>
2026-04-15 22:02:48 +0200

    feat: session creation

--- context/session_store.go
+++ context/session_store.go
@@ -3,4 +3,5 @@ package context
 type SessionStore interface {
 	GetSession(sessionID int) error
 	GetMessages(sessionID int, limit int, offset int) ([]Message, error)
+	CreateSession() (int, error)
 }
