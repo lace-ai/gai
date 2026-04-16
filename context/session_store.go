8d24816433a43244218c9ca2de66512579f49122
SamuelK0nrad <samuel.ko702@gmail.com>
2026-04-16 19:06:49 +0200

    store/in_memory_session_store/ref: fix getmessages return message order

--- context/session_store.go
+++ context/session_store.go
@@ -2,6 +2,7 @@ package context
 
 type SessionStore interface {
 	GetSession(sessionID int) error
+	// GetMessages returns messages in a session, ordered by created_at desc
 	GetMessages(sessionID int, limit int, offset int) ([]Message, error)
 	CreateSession() (int, error)
 	AddMessages(sessionID int, messages []Message) ([]Message, error)
