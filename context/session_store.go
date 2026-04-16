4bd0410b6fadef6462b95bd4b9c4b73a8e4900a6
SamuelK0nrad <samuel.ko702@gmail.com>
2026-04-16 09:22:10 +0200

    feat: save messages after loop

--- context/session_store.go
+++ context/session_store.go
@@ -4,4 +4,6 @@ type SessionStore interface {
 	GetSession(sessionID int) error
 	GetMessages(sessionID int, limit int, offset int) ([]Message, error)
 	CreateSession() (int, error)
+	AddMessages(sessionID int, messages []Message) ([]Message, error)
+	AddMessage(sessionID int, message Message) (Message, error)
 }
