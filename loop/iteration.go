d57d19f82ad21d652684865c15fcbe5d446bf377
SamuelK0nrad <samuel.ko702@gmail.com>
2026-04-25 18:48:11 +0200

    context/add: message content type tool res error

--- loop/iteration.go
+++ loop/iteration.go
@@ -102,18 +102,18 @@ func (i *Iteration) Messages() []aicontext.Message {
 					Role:    aicontext.RoleAssistant,
 					Content: aicontext.NewToolCallContent(part.ToolReq.ID, string(part.ToolReq.Args)),
 				})
-			}
-			if part.ToolResp != nil && part.ToolReq != nil {
-				if part.ToolResp.Err != nil {
-					msgs = append(msgs, aicontext.Message{
-						Role:    aicontext.RoleTool,
-						Content: aicontext.NewTextContent("Error: " + part.ToolResp.Err.Error()),
-					})
-				} else {
-					msgs = append(msgs, aicontext.Message{
-						Role:    aicontext.RoleTool,
-						Content: aicontext.NewToolResultContent(part.ToolReq.ID, part.ToolResp.Text, false, ""),
-					})
+				if part.ToolResp != nil {
+					if part.ToolResp.Err != nil {
+						msgs = append(msgs, aicontext.Message{
+							Role:    aicontext.RoleTool,
+							Content: aicontext.NewToolResultErrContent(part.ToolReq.ID, part.ToolResp.Err.Error()),
+						})
+					} else {
+						msgs = append(msgs, aicontext.Message{
+							Role:    aicontext.RoleTool,
+							Content: aicontext.NewToolResultContent(part.ToolReq.ID, part.ToolResp.Text, false, ""),
+						})
+					}
 				}
 			}
 		case IterationTypeResponse:
